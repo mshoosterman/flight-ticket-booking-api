e4edf9d0ae7be70696834b081ea398c31c13088a
Martin Shoosterman <mshoosterman@d6:42:92:d0:ba:d3.home>
2026-06-16 18:03:58 -0400

    Initial Spring Boot project generated from the Spring Initializer

---
+++ src/main/java/com/mshoosterman/flightbookingapi/FlightTicketBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.mshoosterman.flightbookingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTicketBookingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightTicketBookingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mshoosterman/flightbookingapi/FlightTicketBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mshoosterman.flightbookingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightTicketBookingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
